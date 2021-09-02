4e057fdd128895accfec8ae3313cb3c1e8ab730a
darshansatra1 <darshansatra1@gmail.com>
2021-09-02 11:22:25 +0530

    added tracking code

--- index.php
+++ index.php
@@ -13,6 +13,16 @@
     <title>
         Nike
     </title>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-206582333-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-206582333-1');
+    </script>
+
 </head>
 
 <body>
